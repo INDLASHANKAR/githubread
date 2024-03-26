93d7edcfaafa7268d76822747d9706b477fa535c
indlashankar <indlashankar381@gmail.com>
2024-03-26 18:36:11 +0530

    Added a new comment

--- src/test/java/test/Demo.java
+++ src/test/java/test/Demo.java
@@ -9,7 +9,7 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
-
+//this a demo
 public class Demo {
 public static void main(String[] args) throws IOException {
 	//ExtentReports extent = new ExtentReports();
